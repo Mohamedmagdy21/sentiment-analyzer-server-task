342efc15149423fbc47c22a7fd37f36765781894
Mohamedmagdy21 <mohamedmagdy10011@gmail.com>
2026-05-13 23:26:13 +0300

    Add deploy debug logs

--- deploy.py
+++ deploy.py
@@ -8,8 +8,13 @@ deployment = Deployment(
    user=os.getenv("LIGHTNING_USERNAME")
 )
 
+print(os.getenv("TEAMSPACE"))
+print(os.getenv("IMAGE_NAME"))
+print(os.getenv("DOCKERHUB_USERNAME"))
+print(os.getenv("GITHUB_SHA_SHORT"))
+
 # Create the initial release 
 deployment.start(
-   image=f"{os.getenv('DOCKERHUB_USERNAME')}/{os.getenv('IMAGE_NAME')}:{os.getenv('CI_COMMIT_SHORT_SHA')}", # Ollama Docker image
+   image=f"{os.getenv('DOCKERHUB_USERNAME')}/{os.getenv('IMAGE_NAME')}:{os.getenv('GITHUB_SHA')}", # Ollama Docker image
    ports=[8000],
 )
