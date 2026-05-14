bad3c61c30efc6cdf7837c1388ec213800e1671b
Mohamedmagdy21 <mohamedmagdy10011@gmail.com>
2026-05-14 22:37:59 +0300

    Rerun deploy_test_latestt

--- deploy.py
+++ deploy.py
@@ -3,7 +3,7 @@ import os
 from lightning_sdk.deployment import Deployment
 
 deployment = Deployment(
-   name="sentiment-analyzer-othertriallll", 
+   name="sentiment-analyzer-othertrialllll", 
    teamspace=os.getenv("TEAMSPACE"),
    user=os.getenv("LIGHTNING_USER_ID")
    
